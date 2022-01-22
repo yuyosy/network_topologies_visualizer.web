87841f509ab4d437324c07e23551b89a0208cb72
yuyosy <y-ysss@outlook.com>
2022-01-23 04:09:25 +0900

    Add: web/data/topology_data.js

---
+++ web/data/topology_data.js
@@ -0,0 +1,27 @@
+const topologyData = {
+    "links": [
+        {
+            "id": 0,
+            "source": 0,
+            "srcDevice": "RT01",
+            "srcIfName": "Gi0/1",
+            "target": 1,
+            "tgtDevice": "RT02",
+            "tgtIfName": "Gi0/1"
+        }
+    ],
+    "nodes": [
+        {
+            "icon": "router",
+            "id": 0,
+            "name": "RT01",
+            "layerSortPreference": 6
+        },
+        {
+            "icon": "router",
+            "id": 1,
+            "name": "RT02",
+            "layerSortPreference": 6
+        }
+    ]
+}
